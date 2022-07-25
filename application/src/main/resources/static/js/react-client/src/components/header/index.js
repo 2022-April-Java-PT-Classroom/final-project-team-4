664a3fff1d050029f163f8d28332a09090bd2adc
acbluem <acbluem@gmail.com>
2022-07-25 14:46:19 -0400

    Added header to page with page title

---
+++ application/src/main/resources/static/js/react-client/src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './style.module.scss';
+
+const Header = () => {
+  return (
+    <div className={style.header}>
+      <h1>Footopolis</h1>
+    </div>
+  );
+}
+
+export default Header;
