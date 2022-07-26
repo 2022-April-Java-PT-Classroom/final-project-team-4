ee1d1d5dd5da20593a7d69c46d852e77c89a7a0b
acbluem <acbluem@gmail.com>
2022-07-26 16:58:30 -0400

    added trapezoid design, changed button styles and dropdown backgrounds

--- application/src/main/resources/static/js/react-client/src/components/header/index.js
+++ application/src/main/resources/static/js/react-client/src/components/header/index.js
@@ -4,14 +4,15 @@ import style from './style.module.scss';
 const Header = () => {
   return (
     <div className={style.header}>
+      <div className={style.trapezoid}></div>
       <h1>
-        <span className={style.bigger}>F</span>ootopolis
+        <span id={style.bigger}>F</span>ootopolis
       </h1>
       <nav>
 
       <div className={style.dropdown}>
         <button className={style.dropbtn} id={style.firstBtn}>Foot Problems</button>
-        <div className={style.dropdownContent}>
+        <div className={style.dropdownContent} id={style.green}>
           <a href="#">Injuries</a>
           <a href="#">Growths</a>
           <a href="#">Diseases</a>
@@ -20,7 +21,7 @@ const Header = () => {
 
       <div className={style.dropdown}>
         <button className={style.dropbtn}>Foot Solutions</button>
-        <div className={style.dropdownContent}>
+        <div className={style.dropdownContent} id={style.purple}>
           <a href="#">Conventional Treatments</a>
           <a href="#">Alternative Treatments</a>
           <a href="#">Foot Massage Techniques</a>
@@ -29,15 +30,15 @@ const Header = () => {
 
       <div className={style.dropdown}>
         <button className={style.dropbtn}>Your Feet</button>
-        <div className={style.dropdownContent}>
+        <div className={style.dropdownContent} id={style.green}>
           <a href="#">Foot Injury Quiz</a>
           <a href="#">Local Podiatrist Locator</a>
         </div>
       </div>
 
-      <div className={style.dropdown} id={style.lastBtn}>
-        <button className={style.dropbtn}>About Us</button>
-        <div className={style.dropdownContent}>
+      <div className={style.dropdown}>
+        <button className={style.dropbtn} id={style.lastBtn}>About Us</button>
+        <div className={style.dropdownContent} id={style.lastDropdown}>
           <a href="#">Contact Us</a>
           <a href="#">Meet the Developers</a>
         </div>
