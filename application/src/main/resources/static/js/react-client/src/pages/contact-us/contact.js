5d0e9f77a613bff19c1a3a69e7a7a8625df8df71
cmsrimsson <cmsrimsson@gmail.com>
2022-07-27 15:35:19 -0400

    contact-page function for sending message updates

---
+++ application/src/main/resources/static/js/react-client/src/pages/contact-us/contact.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ContactPage from "./index.js";
+ 
+
+const button = document.querySelector("#send");
+
+button.addEventListener('click', function(){
+mail.preventDefault()
+
+ console.log("this is sample massege");
+
+// Email.send({
+//     Host : "user.gmail.com",
+//     Username : "username",
+//     Password : "password",
+//     To : 'them@website.com',
+//     From : "you@isp.com",
+//     Subject : "This is the subject",
+//     Body : "And this is the body"
+// }).then(
+//   message => alert(message)
+// );
+})
---
+++ application/src/main/resources/static/js/react-client/src/pages/contact-us/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import emailjs from "emailjs-com";
+import style from "./style.module.scss";
+
+
+const ContactPage = () => {
+
+    const post = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm("service_7t8gegw", "template_efxnhoi", e.target, "XGpCd75LuoPDxCnTt")
+        // emailjs.sendForm("service_7t8gegw","template_efxnhoi","XGpCd75LuoPDxCnTt")
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset();
+      };
+    
+
+
+    return (
+      <div className={style.contactPage}>
+       
+        <form onSubmit={post}>
+            <input type="text" placeholder="Name" id="name" name="name" autoComplete="on"/>
+            <input type="email" placeholder="Email" id="email" name="email"autoComplete="on"/>
+            <input type="text" placeholder="Subject" id="subject" name="subject"autoComplete="on"/>
+            <textarea type="text" placeholder="Type Your Message Here" id="message" cols="40" rows="20" name="message"></textarea>
+
+            <button id="send">Send</button>
+
+        </form>
+        
+        </div>
+
+    )
+
+}
+export default ContactPage;
