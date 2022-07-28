6ead65bb0553b3f38bc1a79fbd904b46b43e00d9
jjmirwaldjr <jjmirwaldjr@gmail.com>
2022-07-28 15:30:27 -0400

    files created

--- application/src/main/resources/static/js/react-client/src/components/app-router/index.js
+++ application/src/main/resources/static/js/react-client/src/components/app-router/index.js
@@ -1,10 +1,13 @@
 import { Route, Switch } from 'react-router-dom';
+
 import HomeScreen from '../../pages/home-screen';
+import initMap from '../../pages/podiatrist-finder/GgleMap';
 
 const AppRouter = () => {
   return (
       <Switch>
           <Route exact path={'/'} component={HomeScreen} />
+          <Route exact path={'/map'} component={initMap} />
       </Switch>
   );
 }
