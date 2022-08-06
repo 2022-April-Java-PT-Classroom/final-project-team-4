c42d6a75345e167da1621d59af78792844754720
jjmirwaldjr <jjmirwaldjr@gmail.com>
2022-08-06 10:36:32 -0400

    adjusted the map size and added a small margine

--- application/src/main/resources/static/js/react-client/src/pages/podiatrist-finder/BingMap.js
+++ application/src/main/resources/static/js/react-client/src/pages/podiatrist-finder/BingMap.js
@@ -21,13 +21,13 @@ function MyMapApp() {
         }
       }
       bingMapsKey="Ak_-nVsKEjzxTXWPi28M53qN1Pm6bwFSYms9Ix4EISh80YP36lu8THZknQFF_HdN"
-      height="700px"
+      height="650px"
       mapOptions={
         {
         navigationBarMode: "square",
         }
       }
-      width="1500px"
+      width="1300px"
       viewOptions={
         {
           center: { latitude: 39.983334, longitude: -82.983330 },
