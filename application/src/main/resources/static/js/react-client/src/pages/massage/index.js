37537b6c8d24b5d5377bb17ef8b38856d46a8d8b
anderafells <fellsandera@gmail.com>
2022-08-06 17:52:34 -0400

    started massage page. Changed the name and app router info

---
+++ application/src/main/resources/static/js/react-client/src/pages/massage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './style.module.scss';
+
+
+const Massage = () => {
+    return (
+
+        <div className ={style.Massage}>
+                    <h2>Massage Techniques</h2>
+                    </div>
+
+);
+}
+                   
+export default Massage;
