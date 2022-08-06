57eca7449d4c23c82b945e80a5bcb88d581b0b97
jjmirwaldjr <jjmirwaldjr@gmail.com>
2022-08-06 10:15:00 -0400

    got the map to load more information

--- application/src/main/resources/static/js/react-client/src/pages/podiatrist-finder/BingMap.js
+++ application/src/main/resources/static/js/react-client/src/pages/podiatrist-finder/BingMap.js
@@ -1,25 +1,39 @@
+import React, { useEffect, useState } from "react";
+
 import BingMapsReact from "bingmaps-react";
-import React from "react";
 import style from "./style.module.scss";
 
 function MyMapApp() {
+  const [mapReady, setMapReady] = useState(false);
+  
+  useEffect(() => {
+    if (mapReady) {
+      // addPushPin();
+    }
+  }, [mapReady]);
+  
   return (
     <div className={style.bingMap}>
     <BingMapsReact
+      onMapReady = {
+        ({ map }) => {
+        setMapReady(true);
+        }
+      }
       bingMapsKey="Ak_-nVsKEjzxTXWPi28M53qN1Pm6bwFSYms9Ix4EISh80YP36lu8THZknQFF_HdN"
       height="700px"
       mapOptions={
         {
         navigationBarMode: "square",
+        }
       }
-    }
       width="1000px"
       viewOptions={
         {
-        center: { latitude: 39.983334, longitude: -82.983330 },
-        // mapTypeId: "grayscale",
+          center: { latitude: 39.983334, longitude: -82.983330 },
+          // mapTypeId: "grayscale",
+        }
       }
-    }
     />
     </div>
   );
