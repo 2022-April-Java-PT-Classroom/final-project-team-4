f43f3b7e5092ba65558c664abdfe3258c564bb5d
anderafells <fellsandera@gmail.com>
2022-08-07 14:28:18 -0400

    first commit with ailments added and start of css

--- application/src/main/resources/static/js/react-client/src/pages/massage/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/massage/index.js
@@ -7,25 +7,127 @@ const Massage = () => {
 
         <div className ={style.Massage}>
                     <h2>Massage Techniques</h2>
-                    </div>
+                    <div>
+<div className={style.techniques}>
+<div>
+{/* <img src={sade} height="200" width="200"></img> */}
+<h3>Plantar Fasciitis</h3>
+<div class="videos">   
+<a href="https://youtu.be/0RdOFprqVHU" height="200" width="200"></a>
+</div>
+<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in augue neque. Maecenas fermentum facilisis neque, congue 
+    ullamcorper mauris mattis at. Phasellus dictum sollicitudin arcu in elementum. Suspendisse ornare porttitor neque, vel 
+    viverra erat interdum mattis. Nunc elementum at libero ac aliquam. Donec nibh diam, facilisis at tincidunt eu, tincidunt 
+    quis nulla. Nam et tristique nulla. Mauris eget magna sed mi consequat porta. Sed suscipit mi sed neque ullamcorper posuere.
+     Aenean viverra, eros ac fringilla blandit, libero justo placerat urna, eu ultrices dui ligula at enim. Nunc elementum et 
+     lacus ac laoreet.</p>
+     </div>
 
-// // <!-- Feature item -->
-// <div class="container">
-//     {/* <!-- Image --> */}
-//     <div class="container__image">
-//         {/* ... */}
-//     </div>
+     <div>
 
-//     {/* <!-- Right side --> */}
-//     <div class="container__feature">
-//         ...
-//     </div>
-// </div>
+{/* <img src={sade} height="200" width="200"></img> */}
+<h3> Diabetic Neuropathy</h3>
+<div class="videos">   
+<a href="https://github.com/anderafells" height="200" width="200"></a>
+</div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in augue neque. Maecenas fermentum facilisis neque, congue 
+    ullamcorper mauris mattis at. Phasellus dictum sollicitudin arcu in elementum. Suspendisse ornare porttitor neque, vel 
+    viverra erat interdum mattis. Nunc elementum at libero ac aliquam. Donec nibh diam, facilisis at tincidunt eu, tincidunt 
+    quis nulla. Nam et tristique nulla. Mauris eget magna sed mi consequat porta. Sed suscipit mi sed neque ullamcorper posuere.
+     Aenean viverra, eros ac fringilla blandit, libero justo placerat urna, eu ultrices dui ligula at enim. Nunc elementum et 
+     lacus ac laoreet.</p>
+</div>
 
+<div>
+{/* <img src={CM} height="200" width="200"></img> */}
+<h3>Supination</h3>
+<div class="videos">    
+<a href="https://github.com/cmsrimsson" height="200" width="200"></a>
+</div>
+<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in augue neque. Maecenas fermentum facilisis neque, congue 
+    ullamcorper mauris mattis at. Phasellus dictum sollicitudin arcu in elementum. Suspendisse ornare porttitor neque, vel 
+    viverra erat interdum mattis. Nunc elementum at libero ac aliquam. Donec nibh diam, facilisis at tincidunt eu, tincidunt 
+    quis nulla. Nam et tristique nulla. Mauris eget magna sed mi consequat porta. Sed suscipit mi sed neque ullamcorper posuere.
+     Aenean viverra, eros ac fringilla blandit, libero justo placerat urna, eu ultrices dui ligula at enim. Nunc elementum et 
+     lacus ac laoreet.</p>
+</div>
 
-// <!-- Repeated items -->
+<div>
+{/* <img src={jason} height="200" width="200"></img> */}
+<h3>Pronation</h3>
+<div class="videos">    
+<a href="https://github.com/cmsrimsson" height="200" width="200"></a>
+</div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in augue neque. Maecenas fermentum facilisis neque, congue 
+    ullamcorper mauris mattis at. Phasellus dictum sollicitudin arcu in elementum. Suspendisse ornare porttitor neque, vel 
+    viverra erat interdum mattis. Nunc elementum at libero ac aliquam. Donec nibh diam, facilisis at tincidunt eu, tincidunt 
+    quis nulla. Nam et tristique nulla. Mauris eget magna sed mi consequat porta. Sed suscipit mi sed neque ullamcorper posuere.
+     Aenean viverra, eros ac fringilla blandit, libero justo placerat urna, eu ultrices dui ligula at enim. Nunc elementum et 
+     lacus ac laoreet. </p>
+   </div>
+
+   <div>
+{/* <img src={jason} height="200" width="200"></img> */}
+<h3>Heel Spur</h3>
+<div class="videos">   
+<a href="https://github.com/cmsrimsson"height="200" width="200"></a>
+</div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in augue neque. Maecenas fermentum facilisis neque, congue 
+    ullamcorper mauris mattis at. Phasellus dictum sollicitudin arcu in elementum. Suspendisse ornare porttitor neque, vel 
+    viverra erat interdum mattis. Nunc elementum at libero ac aliquam. Donec nibh diam, facilisis at tincidunt eu, tincidunt 
+    quis nulla. Nam et tristique nulla. Mauris eget magna sed mi consequat porta. Sed suscipit mi sed neque ullamcorper posuere.
+     Aenean viverra, eros ac fringilla blandit, libero justo placerat urna, eu ultrices dui ligula at enim. Nunc elementum et 
+     lacus ac laoreet.</p>
+   </div>
+
+<div>
+{/* <img src={zach} height="200" width="200"></img> */}
+<h3>Hammertoe</h3>
+<div class="videos">   
+<a href="https://github.com/cmsrimsson" height="200" width="200"></a>
+</div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in augue neque. Maecenas fermentum facilisis neque, congue 
+    ullamcorper mauris mattis at. Phasellus dictum sollicitudin arcu in elementum. Suspendisse ornare porttitor neque, vel 
+    viverra erat interdum mattis. Nunc elementum at libero ac aliquam. Donec nibh diam, facilisis at tincidunt eu, tincidunt 
+    quis nulla. Nam et tristique nulla. Mauris eget magna sed mi consequat porta. Sed suscipit mi sed neque ullamcorper posuere.
+     Aenean viverra, eros ac fringilla blandit, libero justo placerat urna, eu ultrices dui ligula at enim. Nunc elementum et 
+     lacus ac laoreet.</p>
+    </div>
+
+    <div>
+{/* <img src={zach} height="200" width="200"></img> */}
+<h3>Bunion</h3>
+<div class="videos">   
+<a href="https://github.com/cmsrimsson" height="200" width="200"></a>
+</div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in augue neque. Maecenas fermentum facilisis neque, congue 
+    ullamcorper mauris mattis at. Phasellus dictum sollicitudin arcu in elementum. Suspendisse ornare porttitor neque, vel 
+    viverra erat interdum mattis. Nunc elementum at libero ac aliquam. Donec nibh diam, facilisis at tincidunt eu, tincidunt 
+    quis nulla. Nam et tristique nulla. Mauris eget magna sed mi consequat porta. Sed suscipit mi sed neque ullamcorper posuere.
+     Aenean viverra, eros ac fringilla blandit, libero justo placerat urna, eu ultrices dui ligula at enim. Nunc elementum et 
+     lacus ac laoreet.</p>
+    </div>
+
+    <div>
+{/* <img src={zach} height="200" width="200"></img> */}
+<h3>Morton Neuroma</h3>
+<div class="videos">   
+<a href="https://github.com/cmsrimsson" height="200" width="200"></a>
+</div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in augue neque. Maecenas fermentum facilisis neque, congue 
+    ullamcorper mauris mattis at. Phasellus dictum sollicitudin arcu in elementum. Suspendisse ornare porttitor neque, vel 
+    viverra erat interdum mattis. Nunc elementum at libero ac aliquam. Donec nibh diam, facilisis at tincidunt eu, tincidunt 
+    quis nulla. Nam et tristique nulla. Mauris eget magna sed mi consequat porta. Sed suscipit mi sed neque ullamcorper posuere.
+     Aenean viverra, eros ac fringilla blandit, libero justo placerat urna, eu ultrices dui ligula at enim. Nunc elementum et 
+     lacus ac laoreet.</p>
+    </div>
+</div>
+</div>
+</div>
+
 
 );
 }
+
                    
 export default Massage;
