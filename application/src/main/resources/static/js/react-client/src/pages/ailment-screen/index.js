f7c8108da6565aae335cbfca4311124207fc371a
jjmirwaldjr <jjmirwaldjr@gmail.com>
2022-08-07 16:38:32 -0400

    IT ALL WORKS!!!!!

--- application/src/main/resources/static/js/react-client/src/pages/ailment-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/ailment-screen/index.js
@@ -10,6 +10,7 @@ import style from './style.module.scss';
 const AilmentScreen = () => {
 
     const [ailment, setAilment] = useState(null);
+    const [treatment, setTreatment] = useState(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(()=> {
@@ -19,50 +20,67 @@ const AilmentScreen = () => {
             setAilment(result.data);
         }
         
- 
+
         if(ailment) {
             setLoading(false);
         }
         
         const timer = setTimeout(() => {
             !ailment && fetchAilmentData();
-        }, 1000);
-        
+        }, 2000);
         
            return ()=> clearTimeout(timer);
-        
-        
-        
-        
-
-    
 
 
     }, [ailment]);
 
-        console.log(ailment);
+    useEffect(() => {
+        const fetchTreatmentData = async () =>{
+            const treatmentResult = await Axios ('http://localhost:8080/treatments')
+            setTreatment(treatmentResult.data);
+        }
+        if(treatment) {
+            setLoading(false);
+        }
+        const treatmentTimer = setTimeout(() => {
+            !treatment && fetchTreatmentData();
+        }, 1000);
         
+           return ()=> clearTimeout(treatmentTimer);
+    }, [treatment]);
+
+        // console.log(ailment);
       
 
     return (
         <div>
-            <div className={style.dropdown}>
-                <button className={style.dropDwnBtn}>Ailment's</button>
-                <section className={style.ailmentSection}>
-                    <div className={style.ailmentText}>
-                        {
-                            loading ? <h3>Loading...</h3> : ailment.map(ailment => (
-                                <div key = {ailment.id}>
-                                {/* <a href={`ailment/${ailment.id}` }>{ailment.name}</a> */}
-                                    <h1 className="ailment_name"><a href={`ailment/${ailment.id}` }>{ailment.name}</a></h1>
-                                    <h2 className="ailment_description"><a> {ailment.description}</a></h2>
-                                </div>
-                            ))
-                        }
-                        </div> 
-                </section>
-                </div>
+        <div className={style.dropdown}>
+            <button className={style.dropDwnBtn}>Ailment's</button>
+            <section className={style.ailmentSection}>
+                <div className={style.ailmentText}>
+                    {loading ? <h3>Loading...</h3> : ailment && ailment.map(ailment => (
+                       <div key = {ailment.id}>
+                       <h1 className="ailment_name"><a href={`ailment/${ailment.id}` }>{ailment.name}</a></h1>
+                       <h2 className="ailment_description"><a> {ailment.description}</a></h2>
+                    </div>
+                    ))}
+                    </div> 
+            </section>
+            </div>
             <br></br>
+            <div className={style.dropdown}>
+                <button className={style.dropDwnBtn}>Treaments's</button>
+                <section className={style.treatmentSection}>
+                    <div className={style.treatmentText}>
+                        {loading ? <h3>Loading...</h3> : treatment && treatment.map(treatment => (
+                        <div key = {treatment.id}>
+                        <h1 className="treatment_name"><a href={`treatments/${treatment.id}` }>{treatment.name}</a></h1>
+                        {/* <h2 className="treatment_description"><a> {treatment.description}</a></h2> */}
+                        </div>
+                        ))}
+                    </div> 
+                </section>
+            </div>
         </div>
         
     )
