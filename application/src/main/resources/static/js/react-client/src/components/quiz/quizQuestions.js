133a24b68a346c7504617140e8089547d2041f89
acbluem <acbluem@gmail.com>
2022-08-11 14:27:56 -0400

    made quizQuestions component

---
+++ application/src/main/resources/static/js/react-client/src/components/quiz/quizQuestions.js
@@ -0,0 +1,193 @@
+const quizQuestions = [
+  {
+    question: "Are you experiencing constant pain?",
+    answers: [
+      {
+        type: ["Plantar Fasciitis", 
+                "Diabetic Neuropathy", 
+                "Supination", 
+                "Pronation", 
+                "Heel Spur", 
+                "Hammertoe", 
+                "Morton Neuroma"], 
+        content: "Yes"
+      }, 
+      {
+        type: "None",
+        content: "No"
+      }
+    ]
+  }, 
+  {
+    question: "Do you have any swelling?", 
+    answers: [
+      {
+        type: ["Plantar Fasciitis", 
+                "Diabetic Neuropathy", 
+                "Supination", 
+                "Heel Spur", 
+                "Hammertoe", 
+                "Bunion", 
+                "Morton Neuroma"], 
+        content: "Yes"
+      }, 
+      {
+        type: "None", 
+        content: "No"
+      }
+    ]
+  }, 
+  {
+    question: "Are you experiencing any numbness or tingling sensation in your feet?",
+    answers: [
+      {
+        type: ["Plantar Fasciitis", 
+                "Diabetic Neuropathy", 
+                "Morton Neuroma"],
+        content: "Yes"
+      },
+      {
+        type: "None",
+        content: "No"
+      }
+    ]
+  },
+  {
+    question: "Do you have any corns or calluses on your feet?",
+    answers: [
+      {
+        type: ["Supination",
+                "Pronation", 
+                "Hammertoe", 
+                "Bunion"],
+        content: "Yes"
+      },
+      {
+        type: "None",
+        content: "No"
+      }
+    ]
+  },
+  {
+    question: "Do you have a protrusion on your foot?",
+    answers: [
+      {
+        type: ["Heel Spur", 
+                "Bunion"],
+        content: "Yes"
+      },
+      {
+        type: "None",
+        content: "No"
+      }
+    ]
+  },
+  {
+    question: "Do you have limited joint mobility?",
+    answers: [
+      {
+        type: ["Hammertoe",
+                "Bunion"],
+        content: "Yes"
+      },
+      {
+        type: "None",
+        content: "No"
+      }
+    ]
+  },
+  {
+    question: "Do you have a burning sensation in your feet?",
+    answers: [
+      {
+        type: ["Diabetic Neuropathy", 
+                "Heel Spur"],
+        content: "No"
+      },
+      {
+        type: "None",
+        content: "No"
+      }
+    ]
+  },
+  {
+    question: "Are you experiencing any muscle weakness?",
+    answers: [
+      {
+        type: "Diabetic Neuropathy",
+        content: "Yes"
+      },
+      {
+        type: "None",
+        content: "No"
+      }
+    ]
+  },
+  {
+    question: "Do you have a tight Achilles tendon?",
+    answers: [
+      {
+        type: "Plantar Fasciitis",
+        content: "Yes"
+      },
+      {
+        type: "None",
+        content: "No"
+      }
+    ]
+  },
+  {
+    question: "Are your feet sensitive to touch?",
+    answers: [
+      {
+        type: "Diabetic Neuropathy",
+        content: "Yes"
+      },
+      {
+        type: "None",
+        content: "No"
+      }
+    ]
+  },
+  {
+    question: "Do you have flat feet?",
+    answers: [
+      {
+        type: "Pronation",
+        content: "Yes"
+      },
+      {
+        type: "None",
+        content: "No"
+      }
+    ]
+  },
+  {
+    question: "Do you experience a sensation of having a rock under your foot when standing?",
+    answers: [
+      {
+        type: "Morton Neuroma",
+        content: "Yes"
+      },
+      {
+        type: "None",
+        content: "No"
+      }
+    ]
+  },
+  {
+    question: "Do have a sprain or shin splints?",
+    answers: [
+      {
+        type: "Supination",
+        content: "Yes"
+      },
+      {
+        type: "None",
+        content: "No"
+      }
+    ]
+  }
+];
+
+export default quizQuestions;
