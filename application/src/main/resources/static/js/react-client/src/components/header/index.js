4a5163fca6d977ccf3b9a81ee6eb80037e25311a
acbluem <acbluem@gmail.com>
2022-08-13 09:34:39 -0400

    changed header but needs icon yet

--- application/src/main/resources/static/js/react-client/src/components/header/index.js
+++ application/src/main/resources/static/js/react-client/src/components/header/index.js
@@ -5,37 +5,37 @@ import style from './style.module.scss';
 const Header = () => {
   return (
     <div className={style.header}>
-      <div className={style.trapezoid}></div>
-      <h1>
-        <NavLink to={'/'}>
-          <span id={style.bigger}>F</span>ootopolis
-        </NavLink>
-      </h1>
       <nav>
-
-      <div className={style.dropdown}>
-        <button className={style.dropbtn} id={style.firstBtn}>Ailments & Treatments</button>
-        <div className={style.dropdownContent} id={style.green}>
-          <NavLink to={'/ailment'}>Ailments & Suggested Treatment Options</NavLink>
-          <NavLink to={'/massage'}>Foot Massage Techniques</NavLink> 
+        <div className={style.title}>
+          <NavLink to={'/'}>Footopolis
+          </NavLink>
         </div>
-      </div>
 
-      <div className={style.dropdown}>
-        <button className={style.dropbtn}>Your Feet</button>
-        <div className={style.dropdownContent} id={style.green}>
-          <NavLink to={'/quiz'}>Foot Ailment Quiz</NavLink>
-          <NavLink to={'/map'}>Local Podiatrist Locator</NavLink>
+        <div className={style.headerNav}>
+        <div className={style.dropdown}>
+          <button className={style.dropbtn} id={style.firstBtn}>Ailments & Treatments</button>
+          <div className={style.dropdownContent} id={style.purple}>
+            <NavLink to={'/ailment'}>Ailments & Suggested Treatment Options</NavLink>
+            <NavLink to={'/massage'}>Foot Massage Techniques</NavLink> 
+          </div>
         </div>
-      </div>
 
-      <div className={style.dropdown}>
-        <button className={style.dropbtn} id={style.lastBtn}>About Us</button>
-        <div className={style.dropdownContent} id={style.lastDropdown}>
-          <NavLink to={'/contact'}>Contact Us</NavLink>
-          <NavLink to={'/about-us'}>Meet the Developers</NavLink>
+        <div className={style.dropdown}>
+          <button className={style.dropbtn}>Your Feet</button>
+          <div className={style.dropdownContent} id={style.green}>
+            <NavLink to={'/quiz'}>Foot Ailment Quiz</NavLink>
+            <NavLink to={'/map'}>Local Podiatrist Locator</NavLink>
+          </div>
         </div>
-      </div>
+
+        <div className={style.dropdown}>
+          <button className={style.dropbtn} id={style.lastBtn}>About Us</button>
+          <div className={style.dropdownContent} id={style.lastDropdown}>
+            <NavLink to={'/contact'}>Contact Us</NavLink>
+            <NavLink to={'/about-us'}>Meet the Developers</NavLink>
+          </div>
+        </div>
+        </div>
 
       </nav>
     </div>
