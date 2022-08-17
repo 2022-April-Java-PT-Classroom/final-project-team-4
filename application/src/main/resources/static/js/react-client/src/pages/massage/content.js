f57bb17f419882131c35f73541992d6c46c478a5
anderafells <fellsandera@gmail.com>
2022-08-17 16:33:26 -0400

    started change of massage page layout

---
+++ application/src/main/resources/static/js/react-client/src/pages/massage/content.js
@@ -0,0 +1,43 @@
+export const massageTechniques = [
+  {
+    name: "Plantar Fasciitis",
+    videoUrl: "https://www.youtube.com/embed/0RdOFprqVHU",
+    text: "Description Here"
+  },
+  {
+    name: "Diabetic Neuropathy",
+    videoUrl: "https://www.youtube.com/embed/M9ulZYFkhEE",
+    text: "Description Here"
+  },
+  {
+    name: "Supination",
+    videoUrl: "https://www.youtube.com/embed/0RdOFprqVHU",
+    text: "Description Here"
+  },
+  {
+    name: "Pronation",
+    videoUrl: "https://www.youtube.com/embed/cJQSmO_XI6o",
+    text: "Description Here"
+  },
+  {
+    name: "Heel Spur",
+    videoUrl: "https://www.youtube.com/embed/H6dEXAFKPO8",
+    text: "Description Here"
+  },
+  {
+    name: "Hammertoe",
+    videoUrl: "https://www.youtube.com/embed/M7RlDGU9QZA",
+    text: "Description Here"
+  },
+  {
+    name: "Bunion",
+    videoUrl: "https://www.youtube.com/embed/RxmkMWawhf4",
+    text: "Description Here"
+  },
+  {
+    name: "Morton Neuroma",
+    videoUrl: "https://www.youtube.com/embed/XsQhGOzPggc",
+    text: "Description Here"
+  },
+
+];
--- application/src/main/resources/static/js/react-client/src/pages/massage/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/massage/index.js
@@ -1,62 +1,42 @@
-import React from 'react';
-import style from './style.module.scss';
-import massagefoot from '../../assets/images/massagefoot.jpg';
+import React, { useState } from 'react';
 
+import { massageTechniques } from './content';
+import massagefoot from '../../assets/images/massagefoot.jpg';
+import style from './style.module.scss';
 
 const Massage = () => {
+
+
+    const [massageListIndex, setMassageListIndex] = useState(0);
+
+    const changeMassageListItem = (index) => {
+        setMassageListIndex(index);
+    }
+
     return (
-
-        <div className ={style.Massage}>
-        
-                <div>               
-        <img src={massagefoot} className={style.heroImage}></img>
-        <div className={style.centered}>Massage Techniques</div>
-      </div>
+        <div className={style.Massage}>
+            <div>
+                <img src={massagefoot} className={style.heroImage} alt=''></img>
+                <div className={style.centered}>Massage Techniques</div>
+            </div>
+            <div>
+                {massageTechniques.map((massageTechnique, index) => (
+                    <button onClick={() => changeMassageListItem(index)}>{massageTechnique.name}</button>
+                ))}
+                <div className={style.techniques}>
                     <div>
-<div className={style.techniques}>
-<div>
-
-<h3>Scroll To See More</h3> 
-<br></br>
-<p> Plantar Fasciitis</p>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/0RdOFprqVHU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-<br></br>
-<p>Diabetic Neuropathy</p>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/M9ulZYFkhEE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-<br></br>
-<p>Supination</p>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/uEUSP6kUrrk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-<br></br>
-<p>Pronation </p>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/cJQSmO_XI6o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-<br></br>
-<p>Heel Spur</p>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/H6dEXAFKPO8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-<br></br>
-<p>Hammertoe</p>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/M7RlDGU9QZA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-<br></br>
-<p>Bunion</p>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/RxmkMWawhf4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-<br></br>
-<p>Morton Neuroma</p>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/XsQhGOzPggc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-    </div>
-</div>
-</div>
- </div> 
+                        {/* <img src={} height="200" width="200"></img> */}
+                        <h3>{massageTechniques[massageListIndex].name}</h3>
+                        <div class="videos">
+                            <iframe width="560" height="315" src={massageTechniques[massageListIndex].videoUrl} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                        <p> {massageTechniques[massageListIndex].text}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 
-);
-  } 
-
-                   
 export default Massage;
